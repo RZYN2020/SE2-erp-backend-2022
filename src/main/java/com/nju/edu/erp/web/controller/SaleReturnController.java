25e58bf1cc6281a8b048dbde8b634beccf858993
zyx <370349370@qq.com>
2022-06-28 23:33:07 +0800

    fix(SalesReturn Module): fix all bugs

--- src/main/java/com/nju/edu/erp/web/controller/SaleReturnController.java
+++ src/main/java/com/nju/edu/erp/web/controller/SaleReturnController.java
@@ -55,11 +55,11 @@ public class SaleReturnController {
   @GetMapping(value = "/first-approval")
   @Authorized(roles = {Role.SALE_MANAGER, Role.ADMIN})
   public Response firstApproval(
-      @RequestParam("saleSheetId") String saleSheetId,
+      @RequestParam("id") String id,
       @RequestParam("state") SaleReturnSheetState state) {
     if (state.equals(SaleReturnSheetState.FAILURE)
         || state.equals(SaleReturnSheetState.PENDING_LEVEL_2)) {
-      saleReturnService.approval(saleSheetId, state);
+      saleReturnService.approval(id, state);
       return Response.buildSuccess();
     } else {
       return Response.buildFailed("000000", "操作失败");
@@ -73,10 +73,10 @@ public class SaleReturnController {
    */
   @Authorized (roles = {Role.GM, Role.ADMIN})
   @GetMapping(value = "/second-approval")
-  public Response secondApproval(@RequestParam("saleSheetId") String saleSheetId,
+  public Response secondApproval(@RequestParam("id") String id,
       @RequestParam("state") SaleReturnSheetState state)  {
     if(state.equals(SaleSheetState.FAILURE) || state.equals(SaleSheetState.SUCCESS)) {
-      saleReturnService.approval(saleSheetId, state);
+      saleReturnService.approval(id, state);
       return Response.buildSuccess();
     } else {
       return Response.buildFailed("000000","操作失败"); // code可能得改一个其他的
