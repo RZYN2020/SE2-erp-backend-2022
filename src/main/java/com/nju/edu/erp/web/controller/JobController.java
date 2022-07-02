deec11b3e23698aea2826b49791bc4623f93e452
Doom Graham <1399075083@qq.com>
2022-07-02 20:55:54 +0800

    "第二个用例 前两个接口"

--- src/main/java/com/nju/edu/erp/web/controller/JobController.java
+++ src/main/java/com/nju/edu/erp/web/controller/JobController.java
@@ -1,5 +1,44 @@
 package com.nju.edu.erp.web.controller;
 
-public class JobController {
 
+import com.nju.edu.erp.model.po.JobPO;
+import com.nju.edu.erp.model.vo.JobVO;
+import com.nju.edu.erp.service.JobService;
+import com.nju.edu.erp.web.Response;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/job")
+public class JobController {
+    private final JobService jobService;
+
+    @Autowired
+    public JobController(JobService jobService) {
+        this.jobService = jobService;
+    }
+
+    @GetMapping("/findAllJob")
+    public Response findAll() {
+        return Response.buildSuccess(jobService.findAll());
+    }
+
+    @PostMapping("/updateJob")
+    public Response updateJob(@RequestBody JobVO jobVO) {
+        JobPO jobPO = new JobPO();
+        BeanUtils.copyProperties(jobVO, jobPO);
+        jobService.updateJob(jobPO);
+        return Response.buildSuccess();
+    }
+
+    @GetMapping("/findAllCalculateMethod")
+    public Response findAllCalculateMethod() {
+        return null;
+    }
+
+    @GetMapping("/findAllPaymentMethod")
+    public Response findAllPaymentMethod() {
+        return null;
+    }
 }
