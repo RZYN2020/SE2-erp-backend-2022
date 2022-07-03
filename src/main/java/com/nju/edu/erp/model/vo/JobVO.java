f42f4c3ffddbd7ef5f0217a4d0230f63032b36bb
Doom Graham <1399075083@qq.com>
2022-07-03 16:32:20 +0800

    feat: 出于sql语句中某种不知名的特性，我需要把JobVO和JobPO里的 level 改成 jobLevel

--- src/main/java/com/nju/edu/erp/model/vo/JobVO.java
+++ src/main/java/com/nju/edu/erp/model/vo/JobVO.java
@@ -16,7 +16,7 @@ public class JobVO {
     private String name; // 员工姓名
     private BigDecimal basicSalary; // 基本工资
     private BigDecimal jobSalary; // 岗位工资
-    private Integer level; // 岗位级别, 同EmployeeVO里的job_level
+    private Integer jobLevel; // 岗位级别, 同EmployeeVO里的job_level
     private int calculateMethod; // 薪资计算方式
     private PaymentMethod paymentMethod; // 薪资发放方式(月薪制 年薪制)
     private int tax; // 税务信息(表驱动)
