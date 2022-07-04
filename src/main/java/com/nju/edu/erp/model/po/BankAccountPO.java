a882aeeec96651c21cc1242087b361dc689ce4eb
Doom Graham <1399075083@qq.com>
2022-07-04 16:00:16 +0800

    feat: BankAccount about

---
+++ src/main/java/com/nju/edu/erp/model/po/BankAccountPO.java
@@ -0,0 +1,15 @@
+package com.nju.edu.erp.model.po;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class BankAccountPO {
+    private String accountName;
+    private int amount;
+}
