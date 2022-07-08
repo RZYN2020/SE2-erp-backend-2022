2cd45007adcc66363531681147afb64c91138181
Doom Graham <1399075083@qq.com>
2022-07-08 19:43:08 +0800

    feat: feat #999# 业务逻辑层注释补充

--- src/main/java/com/nju/edu/erp/service/YearEndAwardsService.java
+++ src/main/java/com/nju/edu/erp/service/YearEndAwardsService.java
@@ -7,7 +7,15 @@ import java.util.List;
 
 public interface YearEndAwardsService {
 
+    /**
+     * 获取全部员工年终奖信息
+     * @return List<YearEndAwardsVO>
+     */
     List<YearEndAwardsVO> findAllYearEndSalary();
 
+    /**
+     * 给指定员工发放指定金额年终奖
+     * @param employeeId, awards
+     */
     void establishYearEndAwards(Integer employeeId, BigDecimal awards);
 }
