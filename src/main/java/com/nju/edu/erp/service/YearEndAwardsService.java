fc1c3bbed871f93615ac4cf4cdc7cb0c5eee8cf4
Doom Graham <1399075083@qq.com>
2022-07-08 15:50:24 +0800

    1

--- src/main/java/com/nju/edu/erp/service/YearEndAwardsService.java
+++ src/main/java/com/nju/edu/erp/service/YearEndAwardsService.java
@@ -3,6 +3,7 @@ package com.nju.edu.erp.service;
 import java.math.BigDecimal;
 
 public interface YearEndAwardsService {
+
     BigDecimal getTotalSalaryExceptDecember(Integer employeeId);
 
     void establishYearEndAwards(Integer employeeId, BigDecimal awards);
